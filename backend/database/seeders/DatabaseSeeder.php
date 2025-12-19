721de62a27f319816bd590f06c61735e2208d867
Joas Magalhaes <joassantos30@hotmail.com>
2025-12-19 19:05:13 -0300

    chore(api): atualiza seeder com usuários padrão para teste de perfis

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $adminPerfil = \App\Models\Profile::create(['name' => 'Admin']);
+        $usuarioPerfil = \App\Models\Profile::create(['name' => 'Usuario']);
+        $convidadoPerfil = \App\Models\Profile::create(['name' => 'Convidado']);
 
-        User::factory()->create([
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@teste.com',
+            'password' => bcrypt('123456'),
+            'cpf' => '999.999.999-99',
+            'profile_id' => $adminPerfil->1,
+        ]);
+
+        User::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@teste.com',
+            'password' => bcrypt('123456'),
+            'cpf' => '888.888.888-88',
+            'profile_id' => $usuarioPerfil->id,
+        ]);
+
+        User::create([
+            'name' => 'Guest User',
+            'email' => 'guest@teste.com',
+            'password' => bcrypt('123456'),
+            'cpf' => '777.777.777-77',
+            'profile_id' => $convidadoPerfil->id,
         ]);
     }
 }
