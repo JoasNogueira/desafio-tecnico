da23be2e2d8a83ba0bdf0bf39685159bbe9ab968
Joas Magalhaes <joassantos30@hotmail.com>
2025-12-19 14:45:25 -0300

    feat(api): cria migration de endereço

---
+++ backend/database/migrations/2025_12_19_171614_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->string('number')->nullable();
+            $table->string('complement')->nullable();
+            $table->string('neighborhood')->nullable();
+            $table->string('city');
+            $table->string('state', 2);
+            $table->string('zip', 20)->nullable();
+            $table->string('country')->default('Brasil');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
