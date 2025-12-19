92edf869daf2e7c42a5bdaeb6e233adcc7037c4e
Joas Magalhaes <joassantos30@hotmail.com>
2025-12-19 14:43:20 -0300

    feat(api): adiciona campo de cpf e foreign key de perfil

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -10,11 +10,15 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
+        {
+            Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf')->unique();
+
+            $table->foreignId('profile_id')->constrained('profiles')->onDelete('restrict');
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
