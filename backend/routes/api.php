d040ea414a0ed97d4ecac18af24d32f18318cabb
Joas Magalhaes <joassantos30@hotmail.com>
2025-12-19 19:04:30 -0300

    feat(api): protege rotas com sanctum e adiciona endpoints de auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthController;
 
-// Cria automaticamente as rotas: GET, POST, PUT, DELETE para /users
-Route::apiResource('users', UserController::class);
+// Rota Publica
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rotas Protegidas
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profiles', [ProfileController::class, 'index']);
+
+    // Rotas de Usuários(CRUD)
+    Route::apiResource('users', UserController::class);
+
+});
