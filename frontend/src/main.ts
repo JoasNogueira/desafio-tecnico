16c81211c4eeb22f58d335adafbdf362e7144327
Joas Magalhaes <joassantos30@hotmail.com>
2025-12-21 09:22:41 -0300

    chore(web): instala e configura dependência maska para máscaras de input

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { vMaska } from 'maska/vue'
+
 
 const app = createApp(App)
 
 app.use(router) 
+app.directive('maska', vMaska)
+
 app.mount('#app')
